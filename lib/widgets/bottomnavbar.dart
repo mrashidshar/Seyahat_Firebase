8b763746387a6eb02e61b1655e49b57684e06be3
hanood_tunio <hanood.tunio@gmail.com>
2024-10-09 00:39:17 +0500

    rectangular bottom navigation bar with animated selection indicator

--- lib/widgets/bottomnavbar.dart
+++ lib/widgets/bottomnavbar.dart
@@ -1,37 +1,66 @@
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0; // Track the selected icon's index
+
+  final List<IconData> _icons = [
+    Icons.home,
+    Icons.message,
+    Icons.travel_explore, // Search Guide icon
+    Icons.notifications,
+    Icons.settings,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white, // Background color for the bottom nav bar
-      padding: const EdgeInsets.symmetric(vertical: 5), // Padding around the nav bar
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Space icons evenly
-        children: [
-          IconButton(
-            icon: const Icon(Icons.home, color: Colors.blue), // Home icon
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.person, color: Colors.blue), // Person icon
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.message, color: Colors.blue), // Message icon
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.notifications, color: Colors.blue), // Notification bell icon
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.settings, color: Colors.blue), // Settings icon
-            onPressed: () {},
+      height: 60, // Height of the bottom nav bar
+      decoration: BoxDecoration(
+        color: Colors.white, // Background color
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(0)), // No curved edges for rectangular shape
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(0, -3), // Shadow above the bar
           ),
         ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Space icons evenly
+        children: List.generate(_icons.length, (index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                _selectedIndex = index; // Update the selected index
+              });
+            },
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  _icons[index],
+                  color: _selectedIndex == index ? Colors.blue : Colors.grey,
+                  size: 30, // Icon size
+                ),
+                const SizedBox(height: 4),
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300), // Animation duration
+                  width: _selectedIndex == index ? 40 : 0, // Change width based on selection
+                  height: 2, // Height of the line
+                  color: Colors.blue, // Color of the line
+                ),
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
